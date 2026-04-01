a77f3bbcec890dfad55c658cbae8fb6f4bd2b144
user <akparaliyevikromjon7@gmail.com>
2026-04-01 16:38:21 +0500

    added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import icon from "../../assets/Group.png"
 const Header = () => {
   return (
-    <div>
-      
+    <div className='header'>
+      <div className="logo">
+        <img src={icon} alt="icon" />
+        <button><h1>Modernize</h1></button>
+      </div>
+      <div className="search">
+        
+      </div>
     </div>
   );
 }
