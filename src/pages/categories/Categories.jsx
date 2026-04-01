4103b6ad73c1c5767ed511b259cc4c8dd1b876c9
zerobek3479-ship-it <zerobek3479@gmail.com>
2026-04-01 17:28:53 +0500

    finish

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,17 +1,28 @@
-import DATA from '../../data/data'
-
-
-
-const Categories = () => {
-  return <div>
+import "./categories.css"
+const Categories = ({data}) => {
+  return <div className='containerAll'>
+    <div className="upperdata">
+  <h3 className='title'>Categories</h3>
+    <button className='routebtn'>Add Category</button>
+</div>
     {
-      DATA.map((item,index)=>{
-        (
-          <div className="contain">
+      data.map((item,index)=>{
+        return(
+          <div className="wrap">
 
+          <div key={index} className="container">
+          
+          
+            <img className='img' src={item.img} alt="img" />
+            <h3>{item.text}</h3>
+            <p>{item.undertext}</p> 
+            
+          
           </div>
+            </div>
         )
-      }
+      },
+    )
     },
   </div>;
 };
