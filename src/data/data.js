9c13fadc8f4e4029c77ed14274523a53eacecb40
zerobek3479-ship-it <zerobek3479@gmail.com>
2026-04-01 16:56:18 +0500

    dont finished

---
+++ src/data/data.js
@@ -0,0 +1,47 @@
+ export const data = [
+  {
+    img: "https://avatars.mds.yandex.net/i?id=6af2b555d83693ea233788ee13889622c5b0163b-5320367-images-thumbs&n=13",
+    text:'Men Clothes',
+    undertext:'24 items'
+  },
+  {
+    img:"",
+    text:"Women Clothes",
+    undertext:"12 items"
+  },
+  {
+    img:"",
+    text:"Accessories",
+    undertext:"43 items"
+  },
+  {
+    img:"",
+    text:"Cotton Clothes",
+    undertext:"31 items"
+  },
+  {
+    img:"",
+    text:"Summer Clothes",
+    undertext:"26 items"
+  },
+  {
+    img:"",
+    text:"Wedding Clothes",
+    undertext:"52 items"
+  },
+  {
+    img:"",
+    text:"Spring Collection",
+    undertext:"24 items"
+  },
+  {
+    img:"",
+    text:"Casual Clothes",
+    undertext:"52 items"
+  },
+  {
+    img:"",
+    text:"Hats",
+    undertext:"26 items"
+  },
+];
---
+++ src/pages/categories/Categories.jsx
@@ -0,0 +1,19 @@
+import DATA from '../../data/data'
+
+
+
+const Categories = () => {
+  return <div>
+    {
+      DATA.map((item,index)=>{
+        (
+          <div className="contain">
+
+          </div>
+        )
+      }
+    },
+  </div>;
+};
+
+export default Categories;
